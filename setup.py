098f72079868f8ec0f4e7fb973629827eff3040f
Thomas Schraitle <tom_schr@web.de>
2015-12-14 22:28:52 +0100

    Fix #5: Improve setup.py
    * setup.py:
      - use setuptools always, no need to check for distutils
      - include keywords 'include_package_data', 'zip_safe', and 'classifiers'
    * setup.cfg:
      - add bdist_wheel and sdist section
    * Add missing MANIFEST.in (needed for setup.py dist)
    * Remove executable bit for LICENSE and README.md

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
 
+from setuptools import setup
 from kiwi.version import __VERSION__
 
 config = {
+    'name': 'kiwi',
     'description': 'KIWI - Appliance Builder',
     'author': 'Marcus Schäfer',
     'url': 'https://github.com/openSUSE/kiwi',
@@ -20,7 +20,21 @@ config = {
     'entry_points': {
         'console_scripts': ['kiwi=kiwi.kiwi:main'],
     },
-    'name': 'kiwi'
+
+    'include_package_data': True,
+    'zip_safe': False,
+    'classifiers': [
+       # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+       #'Development Status :: 3 - Alpha',
+       #'Development Status :: 4 - Beta',
+       #'Development Status :: 5 - Production/Stable',
+       'Intended Audience :: Developers',
+       'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+       'Operating System :: POSIX :: Linux',
+       'Programming Language :: Python :: 2.7',
+       'Programming Language :: Python :: 3.3',
+       'Topic :: System :: Operating System',
+    ],
 }
 
 setup(**config)
