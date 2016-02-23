cb579a822bfb2ad98b3f7131d9e0ad1b5db08e8f
Thomas Schraitle <tom_schr@web.de>
2016-02-23 20:58:12 +0100

    Activate personal spellchecking dictionary

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -16,7 +16,7 @@ extensions = [
 spelling_lang='en_US'
 spelling_show_suggestions = True
 spelling_ignore_pypi_package_names = True
-# spelling_word_list_filename='spelling_wordlist.txt'
+spelling_word_list_filename='spelling_wordlist.txt'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['.templates']
