ea01c4fc38257675f58c887e9a4f7001578d1a03
Marcus Schäfer <ms@suse.com>
2016-05-08 19:04:41 +0200

    Load logging at runtime for MountManager

--- kiwi/mount_manager.py
+++ kiwi/mount_manager.py
@@ -21,7 +21,6 @@ from tempfile import mkdtemp
 # project
 from .command import Command
 from .path import Path
-from .logger import log
 
 
 class MountManager(object):
@@ -96,6 +95,7 @@ class MountManager(object):
 
         :rtype: bool
         """
+        from .logger import log
         if self.is_mounted():
             umounted_successfully = False
             for busy in [1, 2, 3]:
