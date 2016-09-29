def6aa32d110d6dce112cd66e3f4c6e4d9619c7a
Marcus Schäfer <ms@suse.com>
2016-09-29 09:28:49 +0200

    Added rpm_excludedocs handling for apt
    Setup the appropriate dpkg options to avoid the installation
    of manual pages and package documentation if rpm_excludedocs
    is set in the kiwi XML description. Related to #133

--- test/unit/repository_template_apt_test.py
+++ test/unit/repository_template_apt_test.py
@@ -19,3 +19,11 @@ class TestPackageManagerTemplateAptGet(object):
 
     def test_get_image_template(self):
         assert self.apt.get_image_template().substitute()
+
+    def test_get_host_template_with_exclude_docs(self):
+        assert self.apt.get_host_template(exclude_docs=True).substitute(
+            apt_shared_base='/var/cache/kiwi/apt-get'
+        )
+
+    def test_get_image_template_with_exclude_docs(self):
+        assert self.apt.get_image_template(exclude_docs=True).substitute()
