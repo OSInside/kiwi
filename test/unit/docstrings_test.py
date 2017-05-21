f30278f262795c5cdcc8897c2679f6928f523a68
Thomas Schraitle <tom_schr@web.de>
2017-05-21 15:32:09 +0200

    Add two new testcases for docstrings
    * test_docstrings_nonempty
      just tests for an non-empty docstring for each KIWI functions
    * test_docstrings_args
      tests , if a docstring contains descriptions of all parameters
      of a function
    
    => Private functions starting with "_" in the function name are
       skipped.

---
+++ test/unit/docstrings_test.py
@@ -0,0 +1,79 @@
+import inspect
+import pkgutil
+import pytest
+import re
+
+import kiwi
+
+
+def members(module, predicate=None):
+    """Yields all functions of a module if predicate=None
+
+    :param module: the given module to search for members
+    :type module: :class:`module`
+    :param predicate: a function with expects an object and returns True or False
+                      see :func:`inspect.isfunction` as an example
+    :type prediate: :class:`function`
+    """
+    predicate = inspect.isfunction if predicate is None else predicate
+    for _, func, in inspect.getmembers(module, predicate):
+        if func.__module__ == module.__name__:
+            yield func
+
+
+def getmodules(module, predicate=None):
+    """Yields recursively sub module names from a given module
+
+    :param module: the given module to search for members
+    :type module: :class:`module`
+    :param predicate: a function with expects an object and returns True or False
+                      see :func:`inspect.isfunction` as an example
+    :type prediate: :class:`function`
+    """
+    prefix = module.__name__ + "."
+    for importer, modname, ispkg in pkgutil.iter_modules(module.__path__, prefix):
+        module = importer.find_module(modname).load_module(modname)
+        yield from members(module, predicate)
+        if ispkg:
+            getmodules(module, predicate)
+        del module
+
+
+def getallfunctions(module):
+    yield from getmodules(module)
+
+
+# Only add modules/functions starting not with "_"; this would indicate a private
+# member.
+modfuncs = [ff for ff in getallfunctions(kiwi) if not ff.__name__.startswith("_")]
+modfuncsnames = ["%s.%s" % (ff.__module__, ff.__name__) for ff in modfuncs]
+
+
+@pytest.mark.parametrize("func",
+                         modfuncs,
+                         ids=modfuncsnames
+                         )
+def test_docstrings_nonempty(func):
+    """Test if docstring is not empty"""
+    fname = func.__name__
+    doc = func.__doc__
+
+    assert doc is not None, "Need an non-empty docstring for %r" % fname
+
+
+@pytest.mark.parametrize("func",
+                         modfuncs,
+                         ids=modfuncsnames
+                         )
+def test_docstrings_args(func):
+    """Test if docstring contains description of all parameters"""
+    fname = ".".join([func.__module__, func.__name__])
+    doc = func.__doc__
+
+    assert doc is not None
+    if func.__code__.co_argcount:
+        for arg in inspect.getargspec(func).args:
+            m = re.search(":param\s+\w*\s*%s:" % arg, doc)
+            assert m, "Func argument %r " \
+                "not explained in docstring " \
+                "of function %r" % (arg, fname)
