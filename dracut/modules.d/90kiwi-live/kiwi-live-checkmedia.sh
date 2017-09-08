21203bb4ef047d85a8408d74ac56fccafa4967a5
Marcus Schäfer <ms@suse.de>
2017-09-08 12:05:45 +0200

    Add mediacheck handler for live iso
    This fixes bsc#1049304

---
+++ dracut/modules.d/90kiwi-live/kiwi-live-checkmedia.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+type getarg >/dev/null 2>&1 || . /lib/dracut-lib.sh
+type runMediaCheck >/dev/null 2>&1 || . /lib/kiwi-live-lib.sh
+
+if getargbool 0 mediacheck; then
+    initGlobalDevices ${root#live:}
+    runMediaCheck
+fi
