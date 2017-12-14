6aa385d2237849bf99db13cf3cddbf43cd0ad311
Marcus Schäfer <ms@suse.de>
2017-12-14 16:45:20 +0100

    Bump version: 9.11.28 → 9.11.29

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -93,7 +93,7 @@ author = u'Marcus Schäfer'
 # built documents.
 #
 # The short X.Y version.
-version = u'9.11.28'
+version = u'9.11.29'
 # The full version, including alpha/beta/rc tags.
 release = version
 
