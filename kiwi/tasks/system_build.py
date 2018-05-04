f442551cb6fe26cdd883d9c4b8f49daef91307ef
David Cassany <dcassany@suse.com>
2018-05-04 10:40:13 +0200

    Some simple code cleaning

--- kiwi/tasks/system_build.py
+++ kiwi/tasks/system_build.py
@@ -173,12 +173,6 @@ class SystemBuildTask(CliTask):
         self.runtime_checker.check_repositories_configured()
         self.runtime_checker.check_image_include_repos_publicly_resolvable()
 
-        package_requests = False
-        if self.command_args['--add-package']:
-            package_requests = True
-        if self.command_args['--delete-package']:
-            package_requests = True
-
         log.info('Preparing new root system')
         system = SystemPrepare(
             self.xml_state,
@@ -193,15 +187,14 @@ class SystemBuildTask(CliTask):
         system.install_system(
             manager
         )
-        if package_requests:
-            if self.command_args['--add-package']:
-                system.install_packages(
-                    manager, self.command_args['--add-package']
-                )
-            if self.command_args['--delete-package']:
-                system.delete_packages(
-                    manager, self.command_args['--delete-package']
-                )
+        if self.command_args['--add-package']:
+            system.install_packages(
+                manager, self.command_args['--add-package']
+            )
+        if self.command_args['--delete-package']:
+            system.delete_packages(
+                manager, self.command_args['--delete-package']
+            )
 
         profile = Profile(self.xml_state)
 
