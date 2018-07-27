a05cc79d5174dc649d5a953036c6a8af9a27ab29
David Cassany <dcassany@suse.de>
2018-07-27 14:31:59 +0200

    Add build tests for CentOS

---
+++ build-tests/x86/test-image-centos/config.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#======================================
+# Functions...
+#--------------------------------------
+test -f /.kconfig && . /.kconfig
+test -f /.profile && . /.profile
+
+#======================================
+# Greeting...
+#--------------------------------------
+echo "Configure image: [$kiwi_iname]..."
+
+#======================================
+# Activate services
+#--------------------------------------
+baseInsertService dracut_hostonly
+
+#======================================
+# Setup default target, multi-user
+#--------------------------------------
+baseSetRunlevel 3
+
+exit 0
