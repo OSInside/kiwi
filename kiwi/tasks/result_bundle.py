OSInside/kiwi

commit 98c9c77a72cc65508638a3d4711e6786a623d72b

Do not replace version from the image name
This commit makes sure that replacing version to version plus the
build id on resulting files happens only on version suffixes. Before
that if image name was including the version string it, this part was
also replaced.

Fixes #787 (bsc#1102868)

kiwi/tasks/result_bundle.py
@@ -100,6 +100,7 @@ class ResultBundleTask(CliTask):
             result_directory + '/kiwi.result'
         )
         image_version = result.xml_state.get_image_version()
+        image_name = result.xml_state.get_image_name()
         ordered_results = OrderedDict(sorted(result.get_results().items()))
 
         # hard link bundle files, compress and build checksum
@@ -109,11 +110,15 @@ class ResultBundleTask(CliTask):
             if result_file.use_for_bundle:
                 bundle_file_basename = os.path.basename(result_file.filename)
                 # The bundle id is only taken into account for image results
-                # which contains the image version in its nane
-                bundle_file_basename = bundle_file_basename.replace(
-                    image_version,
-                    image_version + '-' + self.command_args['--id']
-                )
+                # which contains the image version appended in its file name
+                part_name = list(bundle_file_basename.partition(image_name))
+                bundle_file_basename = ''.join([
+                    part_name[0], part_name[1],
+                    part_name[2].replace(
+                        image_version,
+                        image_version + '-' + self.command_args['--id']
+                    )
+                ])
                 log.info('Creating %s', bundle_file_basename)
                 bundle_file = ''.join(
                     [bundle_directory, '/', bundle_file_basename]

commit 12b1e9c2c8298ddbca07bcb474c910c3383d85bd

Fix result bundle command
This commits fixes a regression introduced in 98c9c77a

kiwi/tasks/result_bundle.py
@@ -100,7 +100,7 @@ class ResultBundleTask(CliTask):
             result_directory + '/kiwi.result'
         )
         image_version = result.xml_state.get_image_version()
-        image_name = result.xml_state.get_image_name()
+        image_name = result.xml_state.xml_data.get_name()
         ordered_results = OrderedDict(sorted(result.get_results().items()))
 
         # hard link bundle files, compress and build checksum

test/unit/tasks_result_bundle_test.py
@@ -34,7 +34,7 @@ class TestResultBundleTask(object):
         self.xml_state.get_image_version = mock.Mock(
             return_value='1.2.3'
         )
-        self.xml_state.get_image_name = mock.Mock(
+        self.xml_state.xml_data.get_name = mock.Mock(
             return_value='test-image'
         )
 
@@ -143,7 +143,7 @@ class TestResultBundleTask(object):
             use_for_bundle=True, compress=False, shasum=False
         )
 
-        self.xml_state.get_image_name = mock.Mock(
+        self.xml_state.xml_data.get_name = mock.Mock(
             return_value='test-1.2.3-image'
         )
 