7561059875a7ebe48849a76a632ff59e36494a77
Marcus Schäfer <ms@suse.de>
2018-11-05 14:03:45 +0100

    Bump version: 9.16.28 → 9.16.29

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -114,7 +114,7 @@ author = u'Marcus Schäfer'
 # built documents.
 #
 # The short X.Y version.
-version = u'9.16.28'
+version = u'9.16.29'
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- kiwi/version.py
+++ kiwi/version.py
@@ -18,5 +18,5 @@
 """
 Global version information used in kiwi and the package
 """
-__version__ = '9.16.28'
+__version__ = '9.16.29'
 __githash__ = '$Format:%H$'
