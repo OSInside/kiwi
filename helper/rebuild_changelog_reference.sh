640284f48cab7efc436256d2f5d1b73f0c12d6b6
Marcus Schäfer <ms@suse.de>
2019-03-15 15:37:05 +0100

    Refactor incremental changelog update
    The creation of the package changelog is based on a reference file.
    However that reference file contained log information in a specific
    timezone which requires to hardcode the region of that timezone
    in the code to correctly run date/time calculations. This can
    be done better from a conceptual point of view. This patch changes
    the handling in a way that the reference file is a git log
    excerpt including the dates as git log lists them. The dates
    contains complete numeric time/date/zone information and can be
    used for calculations. The changelog helper tool converts the
    result data to match the requirements of rpm changelog files
    and prints the time/date information localized to the callers
    timezone or as UTC if the --utc switch is given. By default the
    user local timezone settings applies. That way the setup of
    the local timezone is immaterial to the changelog processor
    and the workaround in the gitlab-ci rpm stage can be deleted
    too.

--- helper/rebuild_changelog_reference.sh
+++ helper/rebuild_changelog_reference.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-helper/update_changelog.py --since package/python-kiwi.changes \
-    > package/python-kiwi.changes && \
-helper/update_changelog.py --until package/python-kiwi.changes \
-    > package/python-kiwi.changes && \
+git log --no-merges --format=fuller > package/python-kiwi.changes
+
 git commit -S -m 'Update changelog reference' \
     package/python-kiwi.changes
