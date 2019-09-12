b03f52e755f3c7729d338b7acd7c47b6286b5cf3
jpwidera <jan.widera@posteo.de>
2019-09-12 10:32:12 +0000

    Reverting partsthat are already implemented
    This problem is discussed in #1195

--- dracut/modules.d/99kiwi-lib/kiwi-lib.sh
+++ dracut/modules.d/99kiwi-lib/kiwi-lib.sh
@@ -41,8 +41,8 @@ function lookup_disk_device_from_root {
         root_device=/dev/$(basename "$(readlink "${root_device}")")
     fi
     for disk_device in $(
-	lsblk -p -n -r -s -o NAME,TYPE "${root_device}" |\
-            grep -E "disk|raid" | cut -f1 -d ' ' | head -n1
+        lsblk -p -n -r -s -o NAME,TYPE "${root_device}" |\
+            grep disk | cut -f1 -d ' '
     ); do
         disk_matches=$((disk_matches + 1))
     done
