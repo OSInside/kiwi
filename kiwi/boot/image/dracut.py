OSInside/kiwi

commit 07ea23a4c4ef132d42e10fc916e29c5a08141fb4

Avoid default installation of dracut kiwi modules
With this commit dracut modules won't be installed by default
in the initrd unless they are requested by the commandline calling
dracut or by a dracut configuration file.

Fixes bsc#1142899 bsc#1136444
Fixes #1103

kiwi/boot/image/dracut.py
@@ -45,6 +45,10 @@ class BootImageDracut(BootImageBase):
         self.dracut_options = []
         self.included_files = []
         self.included_files_install = []
+        self.modules = []
+        self.install_modules = []
+        self.omit_modules = []
+        self.omit_install_modules = []
 
     def include_file(self, filename, install_media=False):
         """
@@ -57,6 +61,58 @@ class BootImageDracut(BootImageBase):
         if install_media:
             self.included_files_install.append('--install')
             self.included_files_install.append(filename)
+
+    def include_module(self, module, install_media=False):
+        """
+        Include module to dracut boot image
+
+        :param string module: module to include
+        :param bool install_media: include the module for install initrds
+        """
+        if install_media and module not in self.install_modules:
+            self.install_modules.append(module)
+        elif module not in self.modules:
+            self.modules.append(module)
+
+    def omit_module(self, module, install_media=False):
+        """
+        Omit module to dracut boot image
+
+        :param string module: module to omit
+        :param bool install_media: omit the module for install initrds
+        """
+        if install_media and module not in self.omit_install_modules:
+            self.omit_install_modules.append(module)
+        elif module not in self.omit_modules:
+            self.omit_modules.append(module)
+
+    def write_system_config_file(self, config, config_file=None):
+        """
+        Writes modules configuration into a dracut configuration file.
+
+        :param dict config: a dictionary containing the modules to add and omit
+        :param string conf_file: configuration file to write
+        """
+        dracut_config = []
+        if not config_file:
+            config_file = os.path.normpath(
+                self.boot_root_directory + Defaults.get_dracut_conf_name()
+            )
+        if config.get('modules'):
+            dracut_config.append(
+                'add_dracutmodules+=" {0} "\n'.format(
+                    ' '.join(config['modules'])
+                )
+            )
+        if config.get('omit_modules'):
+            dracut_config.append(
+                'omit_dracutmodules+=" {0} "\n'.format(
+                    ' '.join(config['omit_modules'])
+                )
+            )
+        if dracut_config:
+            with open(config_file, 'w') as config:
+                config.writelines(dracut_config)
 
     def prepare(self):
         """
@@ -95,9 +151,25 @@ class BootImageDracut(BootImageBase):
                 dracut_initrd_basename = self.initrd_base_name
             if install_initrd:
                 included_files = self.included_files_install
+                modules_args = [
+                    '--add', ' {0} '.format(' '.join(self.install_modules))
+                ] if self.install_modules else []
+                omit_modules_args = [
+                    '--omit', ' {0} '.format(
+                        ' '.join(self.omit_install_modules)
+                    )
+                ] if self.omit_install_modules else []
             else:
                 included_files = self.included_files
+                modules_args = [
+                    '--add', ' {0} '.format(' '.join(self.modules))
+                ] if self.modules else []
+                omit_modules_args = [
+                    '--omit', ' {0} '.format(' '.join(self.omit_modules))
+                ] if self.omit_install_modules else []
             dracut_initrd_basename += '.xz'
+            options = self.dracut_options + modules_args +\
+                omit_modules_args + included_files
             dracut_call = Command.run(
                 [
                     'chroot', self.boot_root_directory,
@@ -105,7 +177,7 @@ class BootImageDracut(BootImageBase):
                     '--no-hostonly',
                     '--no-hostonly-cmdline',
                     '--xz'
-                ] + self.dracut_options + included_files + [
+                ] + options + [
                     dracut_initrd_basename,
                     kernel_details.version
                 ],

test/unit/boot_image_dracut_test.py
@@ -57,6 +57,50 @@ class TestBootImageKiwi:
         ]
         assert self.boot_image.included_files_install == []
 
+    def test_include_module(self):
+        self.boot_image.include_module('foobar')
+        assert self.boot_image.modules == ['foobar']
+        assert self.boot_image.install_modules == []
+
+        self.boot_image.include_module('module', install_media=True)
+        self.boot_image.include_module('foobar')
+        assert self.boot_image.modules == ['foobar']
+        assert self.boot_image.install_modules == ['module']
+
+    def test_omit_module(self):
+        self.boot_image.omit_module('foobar')
+        assert self.boot_image.omit_modules == ['foobar']
+        assert self.boot_image.omit_install_modules == []
+
+        self.boot_image.omit_module('module', install_media=True)
+        self.boot_image.omit_module('foobar')
+        assert self.boot_image.omit_modules == ['foobar']
+        assert self.boot_image.omit_install_modules == ['module']
+
+    def test_write_system_config_file(self):
+        with patch('builtins.open', create=True) as mock_write:
+            self.boot_image.write_system_config_file(
+                config={'modules': ['module'], 'omit_modules': ['foobar']},
+                config_file='/root/dir/my_dracut_conf.conf'
+            )
+            assert call().__enter__().writelines(
+                [
+                    'add_dracutmodules+=" module "\n',
+                    'omit_dracutmodules+=" foobar "\n'
+                ]
+            ) in mock_write.mock_calls
+            assert call(
+                '/root/dir/my_dracut_conf.conf', 'w'
+            ) in mock_write.mock_calls
+
+        with patch('builtins.open', create=True) as mock_write:
+            self.boot_image.write_system_config_file(
+                config={'modules': ['module'], 'omit_modules': ['foobar']},
+            )
+            assert call(
+                'system-directory/etc/dracut.conf.d/02-kiwi.conf', 'w'
+            ) in mock_write.mock_calls
+
     def test_include_file_install(self):
         self.boot_image.include_file('foo', install_media=True)
         assert self.boot_image.included_files == [

dracut/modules.d/90kiwi-dump/kiwi-ramdisk-deployment-generator.sh
@@ -11,6 +11,8 @@ root_uuid=$(
     grep root= | cut -f2- -d=
 )
 
+[ -z "${root_uuid}" ] && exit 1
+
 {
     echo "[Unit]"
     echo "Before=initrd-root-fs.target"

dracut/modules.d/90kiwi-dump/module-setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 # called by dracut
+check() {
+    return 255
+}
+
+# called by dracut
 depends() {
     echo network rootfs-block dm kiwi-lib
     return 0

dracut/modules.d/90kiwi-overlay/module-setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 # called by dracut
+check() {
+    return 255
+}
+
+# called by dracut
 depends() {
     echo rootfs-block dm
     return 0

dracut/modules.d/90kiwi-repart/module-setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 # called by dracut
+check() {
+    return 255
+}
+
+# called by dracut
 depends() {
     echo rootfs-block dm kiwi-lib
     return 0

kiwi/boot/image/base.py
@@ -92,6 +92,48 @@ class BootImageBase:
         """
         pass
 
+    def include_module(self, module, install_media=False):
+        """
+        Include module to boot image
+
+        For kiwi boot no modules configuration is required. Thus in
+        such a case this method is a noop.
+
+        :param string module: module to include
+        :param bool install_media: include the module for install initrds
+        """
+        pass
+
+    def omit_module(self, module, install_media=False):
+        """
+        Omit module to boot image
+
+        For kiwi boot no modules configuration is required. Thus in
+        such a case this method is a noop.
+
+        :param string module: module to omit
+        :param bool install_media: omit the module for install initrds
+        """
+        pass
+
+    def write_system_config_file(
+        self, config, config_file=None
+    ):
+        """
+        Writes relevant boot image configuration into configuration file
+        that will be part of the system image.
+
+        This is used to configure any further boot image rebuilds after
+        deployment. For instance, initrds recreated on kernel update.
+
+        For kiwi boot no specific configuration is required for initrds
+        recreation, thus this method is a noop in that case.
+
+        :param dict config: dictonary including configuration parameters
+        :param string config_file: configuration file to write
+        """
+        pass
+
     def dump(self, filename):
         """
         Pickle dump this instance to a file. If the object dump

kiwi/builder/disk.py
@@ -373,14 +373,18 @@ class DiskBuilder:
         self._write_generic_fstab_to_system_image(device_map)
 
         if self.initrd_system == 'dracut':
-            self._create_dracut_config()
+            if self.root_filesystem_is_multipath is False:
+                self.boot_image.omit_module('multipath')
+            if self.root_filesystem_is_overlay:
+                self.boot_image.include_module('kiwi-overlay')
+                self.boot_image.write_system_config_file(
+                    config={'modules': ['kiwi-overlay']}
+                )
+            if self.build_type_name == 'oem':
+                self.boot_image.include_module('kiwi-repart')
 
         # create initrd cpio archive
         self.boot_image.create_initrd(self.mbrid)
-
-        # create dracut config omitting one time kiwi dracut modules
-        if self.initrd_system == 'dracut':
-            self._create_system_dracut_config()
 
         # create second stage metadata to system image
         self._copy_first_boot_files_to_system_image()
@@ -768,55 +772,6 @@ class DiskBuilder:
         self.disk.map_partitions()
 
         return self.disk.get_device()
-
-    def _create_dracut_config(self):
-        dracut_config = [
-            'hostonly="no"',
-            'dracut_rescue_image="no"'
-        ]
-        dracut_modules = []
-        dracut_modules_omit = ['kiwi-live', 'kiwi-dump']
-        if self.root_filesystem_is_multipath is False:
-            dracut_modules_omit.append('multipath')
-        if self.root_filesystem_is_overlay:
-            dracut_modules.append('kiwi-overlay')
-        else:
-            dracut_modules_omit.append('kiwi-overlay')
-        if self.build_type_name == 'oem':
-            dracut_modules.append('kiwi-lib')
-            dracut_modules.append('kiwi-repart')
-        self._write_dracut_config(
-            config=dracut_config,
-            modules=dracut_modules,
-            omit_modules=dracut_modules_omit
-        )
-
-    def _create_system_dracut_config(self):
-        dracut_modules = []
-        dracut_modules_omit = ['kiwi-live', 'kiwi-dump', 'kiwi-repart']
-        if self.root_filesystem_is_overlay:
-            dracut_modules.append('kiwi-overlay')
-        else:
-            dracut_modules_omit.append('kiwi-overlay')
-        self._write_dracut_config(
-            config=[], modules=dracut_modules, omit_modules=dracut_modules_omit
-        )
-
-    def _write_dracut_config(self, config, modules, omit_modules):
-        dracut_config_file = ''.join(
-            [self.root_dir, Defaults.get_dracut_conf_name()]
-        )
-        if modules:
-            config.append(
-                'add_dracutmodules+=" {0} "'.format(' '.join(modules))
-            )
-        if omit_modules:
-            config.append(
-                'omit_dracutmodules+=" {0} "'.format(' '.join(omit_modules))
-            )
-        with open(dracut_config_file, 'w') as dracut_config:
-            for entry in config:
-                dracut_config.write(entry + os.linesep)
 
     def _write_partition_id_config_to_boot_image(self):
         log.info('Creating config.partids in boot system')

commit 09432f4612c43bea667c8baf6e8b6b56e9509ed7

Fix modules omission for dracut based initrds
This commit fixes a regression introduced in 07ea23a4. In OEM images
the dracut modules were not properly omitted as the code was evaluating
the wrong variable.

Fixes #1201

kiwi/boot/image/dracut.py
@@ -166,7 +166,7 @@ class BootImageDracut(BootImageBase):
                 ] if self.modules else []
                 omit_modules_args = [
                     '--omit', ' {0} '.format(' '.join(self.omit_modules))
-                ] if self.omit_install_modules else []
+                ] if self.omit_modules else []
             dracut_initrd_basename += '.xz'
             options = self.dracut_options + modules_args +\
                 omit_modules_args + included_files

test/unit/boot_image_dracut_test.py
@@ -125,12 +125,15 @@ class TestBootImageKiwi:
         self.boot_image.include_file(
             '/system-directory/var/lib/bar', install_media=True
         )
+        self.boot_image.include_module('foo')
+        self.boot_image.omit_module('bar')
         self.boot_image.create_initrd()
         assert mock_command.call_args_list == [
             call([
                 'chroot', 'system-directory',
                 'dracut', '--force', '--no-hostonly',
                 '--no-hostonly-cmdline', '--xz',
+                '--add', ' foo ', '--omit', ' bar ',
                 '--install', 'system-directory/etc/foo',
                 '--install', '/system-directory/var/lib/bar',
                 'LimeJeOS-openSUSE-13.2.x86_64-1.13.2.initrd.xz', '1.2.3'