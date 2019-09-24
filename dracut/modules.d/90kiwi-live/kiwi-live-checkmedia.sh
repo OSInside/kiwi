fbba0445db72eec71fc2e21191ecebbcb9eac0b4
Marcus Schäfer <ms@suse.de>
2019-09-24 17:48:39 +0200

    Fixed kiwi-live module setup
    The installation of the checkmedia tool is optional and
    and not mandatory. In addition activate the media check
    verification also through the upstream used rd.live.check
    kernel parameter. This is related to Issue #1158

--- dracut/modules.d/90kiwi-live/kiwi-live-checkmedia.sh
+++ dracut/modules.d/90kiwi-live/kiwi-live-checkmedia.sh
@@ -2,7 +2,7 @@
 type getarg >/dev/null 2>&1 || . /lib/dracut-lib.sh
 type runMediaCheck >/dev/null 2>&1 || . /lib/kiwi-live-lib.sh
 
-if getargbool 0 mediacheck; then
+if getargbool 0 mediacheck || getargbool 0 rd.live.check; then
     declare root=${root}
     initGlobalDevices "${root#live:}"
     runMediaCheck
