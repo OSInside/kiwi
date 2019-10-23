56d82845d54ef94fee367df492115e681d1e21df
Marcus Schäfer <ms@suse.de>
2019-10-23 16:01:27 +0200

    Fixed install_devel_packages helper
    Missing libffi and enchant devel packages. Also deleted
    trang since it's not provided by default in the distro
    and optional

--- helper/install_devel_packages.sh
+++ helper/install_devel_packages.sh
@@ -7,8 +7,8 @@ case ${ID} in
         dnf --setopt=install_weak_deps=False install \
             python3 python3-devel 'python3dist(pip)' \
             'python3dist(tox)' make gcc which xz xorriso libxml2-devel \
-            libxslt-devel enchant genisoimage ShellCheck \
-            latexmk texlive-cmap texlive-metafont texlive-ec \
+            libxslt-devel enchant enchant-devel genisoimage ShellCheck \
+            latexmk texlive-cmap texlive-metafont texlive-ec libffi-devel \
             texlive-babel-english texlive-fncychap texlive-fancyhdr \
             texlive-titlesec texlive-tabulary texlive-framed texlive-wrapfig \
             texlive-parskip texlive-upquote texlive-capt-of texlive-needspace \
@@ -18,12 +18,12 @@ case ${ID} in
     opensuse-*|sles)
         zypper install --no-recommends \
             python3-devel python3-tox libxml2-devel libxslt-devel glibc-devel \
-            gcc trang xorriso texlive-fncychap texlive-wrapfig texlive-capt-of \
+            gcc xorriso texlive-fncychap texlive-wrapfig texlive-capt-of \
             texlive-latexmk texlive-cmap texlive-babel-english \
             texlive-times texlive-titlesec texlive-tabulary texlive-framed \
             texlive-float texlive-upquote texlive-parskip texlive-needspace \
             texlive-makeindex-bin texlive-collection-fontsrecommended \
-            texlive-psnfss
+            texlive-psnfss libffi-devel enchant-devel
     ;;
     *)
         echo "Unknown distribution: ${os}"
