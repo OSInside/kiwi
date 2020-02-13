OSInside/kiwi

commit 141f384ec377c55d50eb11ec3a62d3656d2d6381

Add .append file for PXE images
This commit creates the .append file for PXE images. This file
is only created for initrd based images and includes the
UUID of the rootfs and any parameter added by the user within
the 'kernelcmdline' attribute.

Fixes #1327

kiwi/builder/pxe.py
@@ -53,12 +53,14 @@ class PxeBuilder:
         self.target_dir = target_dir
         self.compressed = xml_state.build_type.get_compressed()
         self.xen_server = xml_state.is_xen_server()
+        self.custom_cmdline = xml_state.build_type.get_kernelcmdline()
         self.filesystem = FileSystemBuilder(
             xml_state, target_dir, root_dir + '/'
         )
         self.system_setup = SystemSetup(
             xml_state=xml_state, root_dir=root_dir
         )
+        self.initrd_system = xml_state.get_initrd_system()
 
         self.boot_signing_keys = custom_args['signing_keys'] if custom_args \
             and 'signing_keys' in custom_args else None
@@ -78,6 +80,7 @@ class PxeBuilder:
                 '-' + xml_state.get_image_version()
             ]
         )
+        self.append_file = '{}.append'.format(self.image_name)
         self.archive_name = ''.join([self.image_name, '.tar'])
         self.checksum_name = ''.join([self.image_name, '.md5'])
         self.kernel_filename = None
@@ -172,6 +175,15 @@ class PxeBuilder:
         # create initrd for pxe boot
         self.boot_image_task.create_initrd()
 
+        # create pxe config append information
+        # this information helps to configure the boot server correctly
+        if self.filesystem.root_uuid and self.initrd_system == 'dracut':
+            cmdline = 'root=UUID={}'.format(self.filesystem.root_uuid)
+            if self.custom_cmdline:
+                cmdline += ' {}'.format(self.custom_cmdline)
+            with open(self.append_file, 'w') as append:
+                append.write(cmdline)
+
         # put results into a tarball
         if not self.xz_options:
             self.xz_options = Defaults.get_xz_compression_options()
@@ -180,7 +192,8 @@ class PxeBuilder:
             self.kernel_filename,
             os.path.basename(self.boot_image_task.initrd_filename),
             os.path.basename(self.image),
-            os.path.basename(self.checksum_name)
+            os.path.basename(self.checksum_name),
+            os.path.basename(self.append_file)
         ]
         pxe_tarball = ArchiveTar(
             self.archive_name,

kiwi/builder/filesystem.py
@@ -41,6 +41,7 @@ class FileSystemBuilder:
     **Filesystem image builder**
 
     :param str label: filesystem label
+    :param str root_uuid: UUID of the created filesystem (on block device only)
     :param str root_dir: root directory path name
     :param str target_dir: target directory path name
     :param str requested_image_type: configured image type
@@ -57,6 +58,7 @@ class FileSystemBuilder:
     """
     def __init__(self, xml_state, target_dir, root_dir):
         self.label = None
+        self.root_uuid = None
         self.root_dir = root_dir
         self.target_dir = target_dir
         self.requested_image_type = xml_state.get_build_type_name()
@@ -167,6 +169,7 @@ class FileSystemBuilder:
             self.root_dir + os.sep, self.filesystem_custom_parameters
         )
         filesystem.create_on_device(self.label)
+        self.root_uuid = loop_provider.get_uuid(loop_provider.get_device())
         log.info(
             '--> Syncing data to filesystem on %s', loop_provider.get_device()
         )

test/unit/builder/pxe_test.py
@@ -1,6 +1,6 @@
 from collections import namedtuple
 from mock import (
-    patch, Mock, MagicMock
+    patch, Mock, MagicMock, mock_open
 )
 from pytest import (
     raises, fixture
@@ -29,13 +29,21 @@ class TestPxeBuilder:
         mock_boot.return_value = self.boot_image_task
         self.filesystem = MagicMock()
         self.filesystem.filename = 'myimage.fs'
+        self.filesystem.root_uuid = 'some_uuid'
         mock_filesystem.return_value = self.filesystem
         self.xml_state = Mock()
         self.xml_state.get_image_version = Mock(
             return_value='1.2.3'
         )
+        self.xml_state.get_initrd_system = Mock(
+            return_value='dracut'
+        )
         self.xml_state.xml_data.get_name = Mock(
             return_value='some-image'
+        )
+        self.xml_state.build_type = Mock()
+        self.xml_state.build_type.get_kernelcmdline = Mock(
+            return_value='console=hvc0'
         )
         kernel_type = namedtuple(
             'kernel', ['filename', 'version']
@@ -77,7 +85,18 @@ class TestPxeBuilder:
         self.boot_image_task.required = Mock(
             return_value=True
         )
-        self.pxe.create()
+
+        m_open = mock_open()
+        with patch('builtins.open', m_open, create=True):
+            self.pxe.create()
+
+        m_open.assert_called_once_with(
+            'target_dir/some-image.x86_64-1.2.3.append', 'w'
+        )
+        m_open.return_value.write.assert_called_once_with(
+            'root=UUID=some_uuid console=hvc0'
+        )
+
         self.filesystem.create.assert_called_once_with()
         mock_rename.assert_called_once_with(
             'myimage.fs', 'myimage'
@@ -101,7 +120,14 @@ class TestPxeBuilder:
         tar.create.assert_called_once_with('target_dir')
 
         self.pxe.compressed = False
-        self.pxe.create()
+
+        m_open = mock_open()
+        with patch('builtins.open', m_open, create=True):
+            self.pxe.create()
+
+        m_open.return_value.write.assert_called_once_with(
+            'root=UUID=some_uuid console=hvc0'
+        )
 
         tar.create_xz_compressed.assert_called_once_with(
             'target_dir', xz_options=['--threads=0']

commit 03e46852feb59df6084a53ff07f890a460b79fbc

Fix append file for PXE
This commit fixes a regression introduced in 141f384e. With this commit
the append file is only added to the PXE tarball only when it is
created.

kiwi/builder/pxe.py
@@ -80,7 +80,7 @@ class PxeBuilder:
                 '-' + xml_state.get_image_version()
             ]
         )
-        self.append_file = '{}.append'.format(self.image_name)
+        self.append_file = ''.join([self.image_name, '.append'])
         self.archive_name = ''.join([self.image_name, '.tar'])
         self.checksum_name = ''.join([self.image_name, '.md5'])
         self.kernel_filename = None
@@ -193,8 +193,11 @@ class PxeBuilder:
             os.path.basename(self.boot_image_task.initrd_filename),
             os.path.basename(self.image),
             os.path.basename(self.checksum_name),
-            os.path.basename(self.append_file)
         ]
+
+        if self.filesystem.root_uuid and self.initrd_system == 'dracut':
+            pxe_tarball_files.append(os.path.basename(self.append_file))
+
         pxe_tarball = ArchiveTar(
             self.archive_name,
             create_from_file_list=True,