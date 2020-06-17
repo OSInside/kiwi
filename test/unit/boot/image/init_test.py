46c030d670cd0d8e34b529cdf9ddf482715229c4
Bo Maryniuk <bogdan.maryniuk@elektrobit.com>
2020-06-17 18:20:59 +0200

    Fix tests for BootImage factory

--- test/unit/boot/image/init_test.py
+++ test/unit/boot/image/init_test.py
@@ -17,12 +17,12 @@ class TestBootImage:
     def test_boot_image_not_implemented(self):
         self.xml_state.get_initrd_system.return_value = 'foo'
         with raises(KiwiBootImageSetupError):
-            BootImage(self.xml_state, 'target_dir')
+            BootImage.new(self.xml_state, 'target_dir')
 
     @patch('kiwi.boot.image.BootImageKiwi')
     def test_boot_image_task_kiwi(self, mock_kiwi):
         self.xml_state.get_initrd_system.return_value = 'kiwi'
-        BootImage(self.xml_state, 'target_dir')
+        BootImage.new(self.xml_state, 'target_dir')
         mock_kiwi.assert_called_once_with(
             self.xml_state, 'target_dir', None, None
         )
@@ -30,7 +30,7 @@ class TestBootImage:
     @patch('kiwi.boot.image.BootImageDracut')
     def test_boot_image_task_dracut(self, mock_dracut):
         self.xml_state.get_initrd_system.return_value = 'dracut'
-        BootImage(self.xml_state, 'target_dir', 'root_dir')
+        BootImage.new(self.xml_state, 'target_dir', 'root_dir')
         mock_dracut.assert_called_once_with(
             self.xml_state, 'target_dir', 'root_dir', None
         )
