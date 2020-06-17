51891b188548eb27077ebbafae6c9c520bb19e23
Bo Maryniuk <bogdan.maryniuk@elektrobit.com>
2020-06-17 19:02:23 +0200

    Fix tests for Markup factory

--- test/unit/markup/markup_test.py
+++ test/unit/markup/markup_test.py
@@ -8,12 +8,12 @@ from kiwi.exceptions import KiwiAnyMarkupPluginError
 class TestMarkup:
     @patch('kiwi.markup.MarkupAny')
     def test_MarkupAny(self, mock_MarkupAny):
-        Markup('description')
+        Markup.new('description')
         mock_MarkupAny.assert_called_once_with('description')
 
     @patch('kiwi.markup.MarkupXML')
     @patch('kiwi.markup.MarkupAny')
     def test_MarkupXML(self, mock_MarkupAny, mock_MarkupXML):
         mock_MarkupAny.side_effect = KiwiAnyMarkupPluginError('load-error')
-        Markup('description')
+        Markup.new('description')
         mock_MarkupXML.assert_called_once_with('description')
