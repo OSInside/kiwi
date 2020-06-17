0f8b66e8c128eddd9f9d1b23e6e4e6b2ab688b03
Bo Maryniuk <bogdan.maryniuk@elektrobit.com>
2020-06-17 19:06:47 +0200

    Fix tests for PackageManager factory

--- test/unit/package_manager/init_test.py
+++ test/unit/package_manager/init_test.py
@@ -11,34 +11,34 @@ from kiwi.exceptions import KiwiPackageManagerSetupError
 class TestPackageManager:
     def test_package_manager_not_implemented(self):
         with raises(KiwiPackageManagerSetupError):
-            PackageManager('repository', 'ms-manager')
+            PackageManager.new('repository', 'ms-manager')
 
     @patch('kiwi.package_manager.PackageManagerZypper')
     def test_manager_zypper(self, mock_manager):
         repository = Mock()
-        PackageManager(repository, 'zypper')
+        PackageManager.new(repository, 'zypper')
         mock_manager.assert_called_once_with(repository, None)
 
     @patch('kiwi.package_manager.PackageManagerDnf')
     def test_manager_dnf(self, mock_manager):
         repository = Mock()
-        PackageManager(repository, 'dnf')
+        PackageManager.new(repository, 'dnf')
         mock_manager.assert_called_once_with(repository, None)
 
     @patch('kiwi.package_manager.PackageManagerDnf')
     def test_manager_yum(self, mock_manager):
         repository = Mock()
-        PackageManager(repository, 'yum')
+        PackageManager.new(repository, 'yum')
         mock_manager.assert_called_once_with(repository, None)
 
     @patch('kiwi.package_manager.PackageManagerApt')
     def test_manager_apt(self, mock_manager):
         repository = Mock()
-        PackageManager(repository, 'apt-get')
+        PackageManager.new(repository, 'apt-get')
         mock_manager.assert_called_once_with(repository, None)
 
     @patch('kiwi.package_manager.PackageManagerPacman')
     def test_manager_pacman(self, mock_manager):
         repository = Mock()
-        PackageManager(repository, 'pacman')
+        PackageManager.new(repository, 'pacman')
         mock_manager.assert_called_once_with(repository, None)
