c3ba22bd66c1830c438ce248eb24d9676a60926e
David Cassany <dcassany@suse.com>
2020-07-14 12:30:56 +0200

    Update Ubuntu and Debian image tests
    This commit updates Ubuntu test to Focal distribution and adds an
    additional Debian Buster image test.
    
    Related to #1468

---
+++ build-tests/x86/debian/test-image-iso-oem-vmx/config.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#================
+# FILE          : config.sh
+#----------------
+# PROJECT       : OpenSuSE KIWI Image System
+# COPYRIGHT     : (c) 2006 SUSE LINUX Products GmbH. All rights reserved
+#               :
+# AUTHOR        : Marcus Schaefer <ms@suse.de>
+#               :
+# BELONGS TO    : Operating System images
+#               :
+# DESCRIPTION   : configuration script for Debian based
+#               : operating systems
+#               :
+#               :
+# STATUS        : BETA
+#----------------
+#======================================
+# Functions...
+#--------------------------------------
+test -f /.kconfig && . /.kconfig
+test -f /.profile && . /.profile
+
+#======================================
+# Greeting...
+#--------------------------------------
+echo "Configure image: [$kiwi_iname]..."
+
+#======================================
+# Activate services
+#--------------------------------------
+baseInsertService sshd
+
+#======================================
+# Setup default target, multi-user
+#--------------------------------------
+baseSetRunlevel 3
