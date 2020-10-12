23f7e5025bc2102fe0a1cdd38ba4988484a73e24
Marcus Schäfer <ms@suse.de>
2020-10-12 20:14:34 +0200

    Bump version: 9.21.17 → 9.21.18

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -128,7 +128,7 @@ author = 'Marcus Schäfer'
 # built documents.
 #
 # The short X.Y version.
-version = '9.21.17'
+version = '9.21.18'
 # The full version, including alpha/beta/rc tags.
 release = version
 
