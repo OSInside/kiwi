0d473758bc13a622b9b0aa57c7474fea4de5574d
Marcus Schäfer <ms@suse.de>
2020-11-08 20:43:35 +0100

    Added universal box to build status helper

--- helper/build_status.sh
+++ helper/build_status.sh
@@ -4,6 +4,7 @@ for project in \
     Virtualization:Appliances:SelfContained:fedora \
     Virtualization:Appliances:SelfContained:suse \
     Virtualization:Appliances:SelfContained:ubuntu \
+    Virtualization:Appliances:SelfContained:universal \
     Virtualization:Appliances:Images:Testing_x86:suse \
     Virtualization:Appliances:Images:Testing_x86:centos \
     Virtualization:Appliances:Images:Testing_x86:fedora \
