69498124441b5211a5a1c3a461c05653147c7305
Marcus Schäfer <ms@suse.de>
2020-11-18 13:06:35 +0100

    Fixed dcounter.c flaw report
    Variable scope can be reduced and useless value assignment.

--- tools/dcounter.c
+++ tools/dcounter.c
@@ -9,7 +9,7 @@ int main(int argc, char **argv) {
     const int max_prefix_len = 512;
     char prefix[max_prefix_len];
     unsigned cnt = 0, blk_size = 1024*1024, blk_cnt = 0, size = 0;
-    ssize_t r, w, p;
+    ssize_t r, p;
     char buf[1024*1024];
     argv++; argc--;
     memset (prefix,'\0',max_prefix_len);
@@ -24,8 +24,6 @@ int main(int argc, char **argv) {
     if (argc == 2 && strstr(*argv, "-l")) {
         newline = 1;
         strncpy (prefix, *(argv+1), max_prefix_len - 1);
-        argv += 2;
-        argc -= 2;
     }
     if (!size) {
         fprintf(stderr, "no size in MB set\n");
@@ -34,6 +32,7 @@ int main(int argc, char **argv) {
     fprintf(stderr, "      ");
     while((r = read(0, buf, sizeof buf)) > 0) {
         p = 0;
+        ssize_t w = 0;
         while((w = write(1, buf + p, r - p)) >= 0) {
             p += w;
             if(p >= r) {
