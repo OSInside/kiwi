c91da720be91a85359f83a14aeed428ad354f439
David Cassany <dcassany@suse.com>
2021-01-27 13:40:20 +0100

    Update MicroOS build test
    This commit alignes the MicroOS tests with the MicroOS images build
    for kvm and xen. In addition it adds the installation media request and
    custom initrd modules configuration for the installation media.

--- build-tests/x86/suse/test-image-MicroOS/config.sh
+++ build-tests/x86/suse/test-image-MicroOS/config.sh
@@ -21,32 +21,87 @@
 test -f /.kconfig && . /.kconfig
 test -f /.profile && . /.profile
 
-#======================================
-# Greeting...
-#--------------------------------------
-echo "Configure image: [$kiwi_iname]..."
+set -euxo pipefail
+
+echo "Configure image: [$kiwi_iname]-[$kiwi_profiles]..."
+
+# Systemd controls the console font now
+echo FONT="eurlatgr.psfu" >> /etc/vconsole.conf
 
 #======================================
-# Setup baseproduct link
+# prepare for setting root pw, timezone
 #--------------------------------------
-suseSetupProduct
+echo "** reset machine settings"
+rm -f /etc/machine-id \
+      /var/lib/zypp/AnonymousUniqueId \
+      /var/lib/systemd/random-seed \
+      /var/lib/dbus/machine-id
 
 #======================================
-# Install overlay systemd mount props
+# Specify default systemd target
 #--------------------------------------
-cp /usr/sbin/setup-fstab-for-overlayfs /etc/fstab.script
+baseSetRunlevel multi-user.target
 
 #======================================
-# Activate services
+# Import trusted rpm keys
 #--------------------------------------
-suseInsertService sshd
+suseImportBuildKey
 
 #======================================
-# Setup default target, multi-user
+# Set hostname by DHCP
 #--------------------------------------
-baseSetRunlevel 3
+baseUpdateSysConfig /etc/sysconfig/network/dhcp DHCLIENT_SET_HOSTNAME yes
 
 #======================================
-# SuSEconfig
+# Enable DHCP on eth0
 #--------------------------------------
-suseConfig
+cat >/etc/sysconfig/network/ifcfg-eth0 <<EOF
+BOOTPROTO='dhcp'
+STARTMODE='auto'
+EOF
+
+# Add repos from /etc/YaST2/control.xml
+if [ -x /usr/sbin/add-yast-repos ]; then
+	add-yast-repos
+	zypper --non-interactive rm -u live-add-yast-repos
+fi
+
+# Adjust zypp conf
+sed -i 's/^multiversion =.*/multiversion =/g' /etc/zypp/zypp.conf
+
+#=====================================
+# Configure snapper
+#-------------------------------------
+if [ "${kiwi_btrfs_root_is_snapshot-false}" = 'true' ]; then
+        echo "creating initial snapper config ..."
+        cp /etc/snapper/config-templates/default /etc/snapper/configs/root
+        baseUpdateSysConfig /etc/sysconfig/snapper SNAPPER_CONFIGS root
+
+	# Adjust parameters
+	sed -i'' 's/^TIMELINE_CREATE=.*$/TIMELINE_CREATE="no"/g' /etc/snapper/configs/root
+	sed -i'' 's/^NUMBER_LIMIT=.*$/NUMBER_LIMIT="2-10"/g' /etc/snapper/configs/root
+	sed -i'' 's/^NUMBER_LIMIT_IMPORTANT=.*$/NUMBER_LIMIT_IMPORTANT="4-10"/g' /etc/snapper/configs/root
+fi
+
+#=====================================
+# Enable chrony if installed
+#-------------------------------------
+if [ -f /etc/chrony.conf ]; then
+	systemctl enable chronyd
+fi
+
+
+# To make x-systemd.growfs work from inside the initrd
+cat >/etc/dracut.conf.d/50-microos-growfs.conf <<"EOF"
+install_items+=" /usr/lib/systemd/systemd-growfs "
+EOF
+
+#======================================
+# Disable recommends on virtual images
+#--------------------------------------
+sed -i 's/.*solver.onlyRequires.*/solver.onlyRequires = true/g' /etc/zypp/zypp.conf
+
+#======================================
+# Disable installing documentation
+#--------------------------------------
+sed -i 's/.*rpm.install.excludedocs.*/rpm.install.excludedocs = yes/g' /etc/zypp/zypp.conf
