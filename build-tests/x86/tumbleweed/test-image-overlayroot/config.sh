082bc3c0bbba40c84ae041446e2be937286dc76b
Marcus Schäfer <ms@suse.de>
2021-09-17 11:58:21 +0200

    Fixed Tumbleweed integration tests
    Due to changes in the distribution the tests needs some
    adaptions. The kernel as provided now by TW uses symlinks
    from boot to /usr/lib/modules/... to point to the kernel
    binary. Of course this breaks for any system that uses
    /boot on another partition. Those image tests which were
    testing this aspect needs fixing.

--- build-tests/x86/tumbleweed/test-image-overlayroot/config.sh
+++ build-tests/x86/tumbleweed/test-image-overlayroot/config.sh
@@ -40,3 +40,27 @@ suseInsertService sshd
 # Setup default target, multi-user
 #--------------------------------------
 baseSetRunlevel 3
+
+# For image tests with an extra boot partition the
+# kernel must not be a symlink to another area of
+# the filesystem. Latest changes on SUSE changed the
+# layout of the kernel which breaks every image with
+# an extra boot partition
+#
+# All of the following is more than a hack and I
+# don't like it all
+#
+# Complains and discussions about this please with
+# the SUSE kernel team as we in kiwi can just live
+# with the consequences of this change
+#
+pushd /
+
+for file in /boot/* /boot/.*; do
+    if [ -L ${file} ];then
+        link_target=$(readlink ${file})
+        if [[ ${link_target} =~ usr/lib/modules ]];then
+            mv ${link_target} ${file}
+        fi
+    fi
+done
