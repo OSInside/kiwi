090284db1cef8baa68922509d5701bdebc5a8168
Marcus Schäfer <ms@suse.de>
2021-10-22 17:41:18 +0200

    Bump version: 9.24.1 → 9.24.2

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -140,7 +140,7 @@ author = 'Marcus Schäfer'
 # built documents.
 #
 # The short X.Y version.
-version = '9.24.1'
+version = '9.24.2'
 # The full version, including alpha/beta/rc tags.
 release = version
 
