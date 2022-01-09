OSInside/kiwi

commit 282529de8f612dee32d54ee868c2365dcd829220

Do not force dracut into a compression setting
So far we called dracut with --xz which forces the initrd
to be xz compressed. There are other compression formats
used by the distributions and they might differe from xz.
The selection for a compression tool is done by a dist
configuration in dracut.conf.d which is provided by the
distributions as they see fit. For us this means not
forcing dracut into a specific compression setting allows
to make use of the distro provided setting and also
allows to change/override this setting by an overlay
file. This Fixes bsc#1192975

dracut/modules.d/99kiwi-lib/kiwi-net-lib.sh
@@ -80,7 +80,7 @@ function uri_fragment {
 #--------------------------------------
 function _is_compressed {
     local source_url=$1
-    [[ ${source_url} =~ .xz$ ]]
+    xz -l "${source_url}" &>/dev/null
 }
 
 function _is_dolly {

kiwi/boot/image/dracut.py
@@ -196,7 +196,6 @@ class BootImageDracut(BootImageBase):
             modules_args += [
                 '--omit', ' {0} '.format(' '.join(self.omit_modules))
             ] if self.omit_modules else []
-            dracut_initrd_basename += '.xz'
             options = self.dracut_options + modules_args + included_files
             if kernel_details:
                 self.device_mount = MountManager(
@@ -214,8 +213,7 @@ class BootImageDracut(BootImageBase):
                         'chroot', self.boot_root_directory,
                         'dracut', '--verbose',
                         '--no-hostonly',
-                        '--no-hostonly-cmdline',
-                        '--xz'
+                        '--no-hostonly-cmdline'
                     ] + options + [
                         dracut_initrd_basename,
                         kernel_details.version

kiwi/builder/install.py
@@ -363,7 +363,7 @@ class InstallImageBuilder:
 
     def _create_pxe_install_kernel_and_initrd(self) -> None:
         kernelname = 'pxeboot.{0}.kernel'.format(self.pxename)
-        initrdname = 'pxeboot.{0}.initrd.xz'.format(self.pxename)
+        initrdname = 'pxeboot.{0}.initrd'.format(self.pxename)
         kernel = Kernel(self.boot_image_task.boot_root_directory)
         if kernel.get_kernel():
             kernel.copy_kernel(self.pxe_dir.name, kernelname)

test/unit/boot/image/dracut_test.py
@@ -131,37 +131,43 @@ class TestBootImageKiwi:
             call(device='/proc', mountpoint='system-directory/proc')
         ]
         assert mock_command.call_args_list == [
-            call([
-                'chroot', 'system-directory',
-                'dracut', '--verbose', '--no-hostonly',
-                '--no-hostonly-cmdline', '--xz',
-                '--add', ' foo ', '--omit', ' bar ',
-                '--install', 'system-directory/etc/foo',
-                'LimeJeOS.x86_64-1.13.2.initrd.xz', '1.2.3'
-            ], stderr_to_stdout=True),
-            call([
-                'mv',
-                'system-directory/'
-                'LimeJeOS.x86_64-1.13.2.initrd.xz',
-                'some-target-dir'
-            ])
+            call(
+                [
+                    'chroot', 'system-directory',
+                    'dracut', '--verbose', '--no-hostonly',
+                    '--no-hostonly-cmdline',
+                    '--add', ' foo ', '--omit', ' bar ',
+                    '--install', 'system-directory/etc/foo',
+                    'LimeJeOS.x86_64-1.13.2.initrd', '1.2.3'
+                ], stderr_to_stdout=True),
+            call(
+                [
+                    'mv',
+                    'system-directory/'
+                    'LimeJeOS.x86_64-1.13.2.initrd',
+                    'some-target-dir'
+                ]
+            )
         ]
         mock_command.reset_mock()
         self.boot_image.create_initrd(basename='foo')
         assert mock_command.call_args_list == [
-            call([
-                'chroot', 'system-directory',
-                'dracut', '--verbose', '--no-hostonly',
-                '--no-hostonly-cmdline', '--xz',
-                '--add', ' foo ', '--omit', ' bar ',
-                '--install', 'system-directory/etc/foo',
-                'foo.xz', '1.2.3'
-            ], stderr_to_stdout=True),
-            call([
-                'mv',
-                'system-directory/foo.xz',
-                'some-target-dir'
-            ])
+            call(
+                [
+                    'chroot', 'system-directory',
+                    'dracut', '--verbose', '--no-hostonly',
+                    '--no-hostonly-cmdline',
+                    '--add', ' foo ', '--omit', ' bar ',
+                    '--install', 'system-directory/etc/foo',
+                    'foo', '1.2.3'
+                ], stderr_to_stdout=True),
+            call(
+                [
+                    'mv',
+                    'system-directory/foo',
+                    'some-target-dir'
+                ]
+            )
         ]
 
     def test_has_initrd_support(self):

test/unit/builder/install_test.py
@@ -379,11 +379,11 @@ class TestInstallImageBuilder:
         assert mock_command.call_args_list[1] == call(
             [
                 'mv', 'initrd',
-                'tmpdir/pxeboot.result-image.x86_64-1.2.3.initrd.xz'
+                'tmpdir/pxeboot.result-image.x86_64-1.2.3.initrd'
             ]
         )
         mock_chmod.assert_called_once_with(
-            'tmpdir/pxeboot.result-image.x86_64-1.2.3.initrd.xz', 420
+            'tmpdir/pxeboot.result-image.x86_64-1.2.3.initrd', 420
         )
         mock_archive.assert_called_once_with(
             'target_dir/result-image.x86_64-1.2.3.install.tar'
@@ -409,7 +409,7 @@ class TestInstallImageBuilder:
         ]
         assert mock_chmod.call_args_list == [
             call('tmpdir/result-image.x86_64-1.2.3.initrd', 420),
-            call('tmpdir/pxeboot.result-image.x86_64-1.2.3.initrd.xz', 420)
+            call('tmpdir/pxeboot.result-image.x86_64-1.2.3.initrd', 420)
         ]
         assert m_open.call_args_list == [
             call('tmpdir/result-image.x86_64-1.2.3.append', 'w'),

commit bf29b597fc3ca41b43c7cec05664092c2835d6a8

Fixed regression in compression detection
The change from 282529de8f612dee32d54ee868c2365dcd829220
Introduced a bad regression. The assumption was made that the
xz tool could be used to detect if a file is compressed or not.
However, this requires the file to be locally present. In the
scope of the method call is_compressed() and within a remote
deployment e.g PXE this is not the case. Therefore the former
way to "detect" the compression according to the .xz postfix
of the source filename was restored. In addition the function
name was changed to is_xz_compressed() because that's what the
method can do and not more. This Fixes #2015

dracut/modules.d/99kiwi-lib/kiwi-net-lib.sh
@@ -27,7 +27,7 @@ function fetch_file {
     if _is_dolly "${source_url}";then
         fetch="dolly"
     fi
-    if _is_compressed "${source_url}";then
+    if _is_xz_compressed "${source_url}";then
         fetch="${fetch} | xz -d"
     fi
     if [ -z "${source_uncompressed_bytes}" ];then
@@ -78,9 +78,9 @@ function uri_fragment {
 #======================================
 # Methods considered private
 #--------------------------------------
-function _is_compressed {
+function _is_xz_compressed {
     local source_url=$1
-    xz -l "${source_url}" &>/dev/null
+    [[ ${source_url} =~ .xz$ ]]
 }
 
 function _is_dolly {