OSInside/kiwi

commit 8f2b8fda82a37d7a7ebbf8a6557870cc63b0ae91

Swap from DM kpartx to util-linux partx for creating loop devices
The device-mapper (DM) based loop devices that kiwi has historically
generated creates issues when trying to run kiwi in confined build
environments. The DM tools prefer to have a tighter coupling between
userspace and kernel interfaces, and we cannot necessarily guarantee
that in the variety of build environments that kiwi can be run in.
In particular, Koji uses either nspawn containers or chroots through
Mock to run kiwi and that is where this fails.

However, we do not need to use DM for this purpose when util-linux
provides a perfectly serviceable alternative. This commit changes
kiwi's loop device setup to use partx(8) from util-linux instead.

As long as the appliance image being created doesn't use a disk
scheme that requires DM, it becomes entirely possible to produce
an image without needing DM at runtime at all.

kiwi/storage/disk.py
@@ -400,7 +400,7 @@ class Disk(DeviceProvider):
         """
         if self.storage_provider.is_loop():
             Command.run(
-                ['kpartx', '-s', '-a', self.storage_provider.get_device()]
+                ['partx', '--add', self.storage_provider.get_device()]
             )
             self.is_mapped = True
         else:
@@ -482,7 +482,7 @@ class Disk(DeviceProvider):
         if self.storage_provider.is_loop():
             device_base = os.path.basename(self.storage_provider.get_device())
             device_node = ''.join(
-                ['/dev/mapper/', device_base, 'p', partition_number]
+                ['/dev/', device_base, 'p', partition_number]
             )
         else:
             device = self.storage_provider.get_device()
@@ -503,9 +503,9 @@ class Disk(DeviceProvider):
             log.info('Cleaning up %s instance', type(self).__name__)
             try:
                 for device_node in self.partition_map.values():
-                    Command.run(['dmsetup', 'remove', device_node])
+                    Command.run(['partx', '--delete', device_node])
                 Command.run(
-                    ['kpartx', '-d', self.storage_provider.get_device()]
+                    ['partx', '--delete', self.storage_provider.get_device()]
                 )
             except Exception:
                 log.warning(

test/unit/storage/disk_test.py
@@ -189,14 +189,14 @@ class TestDisk:
     def test_device_map_efi_partition(self, mock_command):
         self.disk.create_efi_partition('100')
         self.disk.map_partitions()
-        assert self.disk.partition_map == {'efi': '/dev/mapper/loop0p1'}
+        assert self.disk.partition_map == {'efi': '/dev/loop0p1'}
         self.disk.is_mapped = False
 
     @patch('kiwi.storage.disk.Command.run')
     def test_device_map_prep_partition(self, mock_command):
         self.disk.create_prep_partition('8')
         self.disk.map_partitions()
-        assert self.disk.partition_map == {'prep': '/dev/mapper/loop0p1'}
+        assert self.disk.partition_map == {'prep': '/dev/loop0p1'}
         self.disk.is_mapped = False
 
     @patch('kiwi.storage.disk.Command.run')
@@ -270,7 +270,7 @@ class TestDisk:
     def test_map_partitions_loop(self, mock_command):
         self.disk.map_partitions()
         mock_command.assert_called_once_with(
-            ['kpartx', '-s', '-a', '/dev/loop0']
+            ['partx', '--add', '/dev/loop0']
         )
         self.disk.is_mapped = False
 
@@ -283,25 +283,25 @@ class TestDisk:
         )
 
     @patch('kiwi.storage.disk.Command.run')
-    def test_destructor_dm_cleanup_failed(self, mock_command):
+    def test_destructor_loop_partition_cleanup_failed(self, mock_command):
         self.disk.is_mapped = True
-        self.disk.partition_map = {'root': '/dev/mapper/loop0p1'}
+        self.disk.partition_map = {'root': '/dev/loop0p1'}
         mock_command.side_effect = Exception
         self.disk.__del__()
         with self._caplog.at_level(logging.WARNING):
             mock_command.assert_called_once_with(
-                ['dmsetup', 'remove', '/dev/mapper/loop0p1']
+                ['partx', '--delete', '/dev/loop0p1']
             )
         self.disk.is_mapped = False
 
     @patch('kiwi.storage.disk.Command.run')
     def test_destructor(self, mock_command):
         self.disk.is_mapped = True
-        self.disk.partition_map = {'root': '/dev/mapper/loop0p1'}
+        self.disk.partition_map = {'root': '/dev/loop0p1'}
         self.disk.__del__()
         assert mock_command.call_args_list == [
-            call(['dmsetup', 'remove', '/dev/mapper/loop0p1']),
-            call(['kpartx', '-d', '/dev/loop0'])
+            call(['partx', '--delete', '/dev/loop0p1']),
+            call(['partx', '--delete', '/dev/loop0'])
         ]
         self.disk.is_mapped = False
 

commit e0212c6006c419d530c398f4f825907f1df50e57

Eliminate redundant partx calls for cleaning up loop devices
partx can clean up all partition loops associated with a loop device
when deleting the main loop device. Apparently, sometimes it goes and
does this even when only deleting the partition loop, so to avoid
this problem, we will just eliminate the redundant call.

Fixes: 8f2b8fda82a37d7a7ebbf8a6557870cc63b0ae91

kiwi/storage/disk.py
@@ -502,8 +502,6 @@ class Disk(DeviceProvider):
         if self.storage_provider.is_loop() and self.is_mapped:
             log.info('Cleaning up %s instance', type(self).__name__)
             try:
-                for device_node in self.partition_map.values():
-                    Command.run(['partx', '--delete', device_node])
                 Command.run(
                     ['partx', '--delete', self.storage_provider.get_device()]
                 )

test/unit/storage/disk_test.py
@@ -283,14 +283,14 @@ class TestDisk:
         )
 
     @patch('kiwi.storage.disk.Command.run')
-    def test_destructor_loop_partition_cleanup_failed(self, mock_command):
+    def test_destructor_loop_cleanup_failed(self, mock_command):
         self.disk.is_mapped = True
         self.disk.partition_map = {'root': '/dev/loop0p1'}
         mock_command.side_effect = Exception
         self.disk.__del__()
         with self._caplog.at_level(logging.WARNING):
             mock_command.assert_called_once_with(
-                ['partx', '--delete', '/dev/loop0p1']
+                ['partx', '--delete', '/dev/loop0']
             )
         self.disk.is_mapped = False
 
@@ -300,7 +300,6 @@ class TestDisk:
         self.disk.partition_map = {'root': '/dev/loop0p1'}
         self.disk.__del__()
         assert mock_command.call_args_list == [
-            call(['partx', '--delete', '/dev/loop0p1']),
             call(['partx', '--delete', '/dev/loop0'])
         ]
         self.disk.is_mapped = False