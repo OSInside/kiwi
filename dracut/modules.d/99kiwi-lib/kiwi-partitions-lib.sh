b12ebcedb7cb18e05ce6a19e3312c16f5c5529a9
David Cassany <dcassany@suse.com>
2023-04-03 13:24:36 +0200

    Expand DASD partition table with parted
    Signed-off-by: David Cassany <dcassany@suse.com>

--- dracut/modules.d/99kiwi-lib/kiwi-partitions-lib.sh
+++ dracut/modules.d/99kiwi-lib/kiwi-partitions-lib.sh
@@ -137,6 +137,13 @@ function create_dasd_partitions {
     local ignore_cmd=0
     local ignore_cmd_once=0
     local cmd
+
+    # We resize first partition to enfore rewrite of the current
+    # partition table updated to the actual disk geometry. This is
+    # to circumvent the fdasd limitation of not being capable to
+    # expand the partition table up to the disk size bsc#1209247
+    parted --script --machine "${disk_device}" resizepart 1
+
     for cmd in ${partition_setup};do
         if [ "${ignore_cmd}" = 1 ] && echo "${cmd}" | grep -qE '[dntwq]';then
             ignore_cmd=0
@@ -278,7 +285,9 @@ function get_free_disk_bytes {
 }
 
 function get_partition_table_type {
-    blkid -s PTTYPE -o value "$1"
+    local disk_device="$1"
+    
+    parted --script --machine "${disk_device}" print | grep "^${disk_device}" | cut -d":" -f6
 }
 
 function get_partition_uuid {
