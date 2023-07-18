cfb4c8edeaae17c3bfb8266fc836b9d6e4200100
Jesus Bermudez Velazquez <jesus.bv@suse.com>
2023-07-18 14:45:23 +0100

    Add SECURE_BOOT no when the firmware is efi
    This Fixes bsc#1211102

--- test/unit/bootloader/config/grub2_test.py
+++ test/unit/bootloader/config/grub2_test.py
@@ -762,6 +762,47 @@ class TestBootLoaderConfigGrub2:
         ]
 
     @patch('os.path.exists')
+    @patch('kiwi.bootloader.config.grub2.SysConfig')
+    def test_setup_sysconfig_bootloader_no_secure(
+        self, mock_sysconfig, mock_exists
+    ):
+        sysconfig_bootloader = MagicMock()
+        mock_sysconfig.return_value = sysconfig_bootloader
+        mock_exists.return_value = True
+        self.bootloader._setup_sysconfig_bootloader()
+        mock_sysconfig.assert_called_once_with(
+            'root_dir/etc/sysconfig/bootloader'
+        )
+        sysconfig_bootloader.write.assert_called_once_with()
+        assert sysconfig_bootloader.__setitem__.call_args_list == [
+            call('DEFAULT_APPEND', '"some-cmdline root=UUID=foo"'),
+            call(
+                'FAILSAFE_APPEND',
+                '"some-cmdline root=UUID=foo failsafe-options"'
+            ),
+            call('LOADER_LOCATION', 'mbr'),
+            call('LOADER_TYPE', 'grub2'),
+            call('TRUSTED_BOOT', 'yes')
+        ]
+        self.firmware.efi_mode = Mock(
+            return_value='efi'
+        )
+        sysconfig_bootloader.__setitem__.reset_mock()
+        self.bootloader._setup_sysconfig_bootloader()
+        print(sysconfig_bootloader.__setitem__.call_args_list)
+        assert sysconfig_bootloader.__setitem__.call_args_list == [
+            call('DEFAULT_APPEND', '"some-cmdline root=UUID=foo"'),
+            call(
+                'FAILSAFE_APPEND',
+                '"some-cmdline root=UUID=foo failsafe-options"'
+            ),
+            call('LOADER_LOCATION', 'none'),
+            call('LOADER_TYPE', 'grub2-efi'),
+            call('SECURE_BOOT', 'no'),
+            call('TRUSTED_BOOT', 'yes')
+        ]
+
+    @patch('os.path.exists')
     def test_setup_live_image_config_custom_template(self, mock_exists):
         bootloader = Mock()
         bootloader.get_grub_template.return_value = "example.template"
