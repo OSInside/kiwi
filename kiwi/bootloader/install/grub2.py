673dcb8fe048fb7a730b42c72e208844ab4f63d6
Steffen Winterfeldt <wfeldt@opensuse.org>
2024-05-02 16:08:35 +0200

    Fix set_disk_password() to really add the password to grub.cfg.
    Since commit 8aa517eb7 it is necessary to call _mount_device_and_volumes()
    in methods that want to modify the grub setup to ensure the current boot
    files are mounted.
    
    Also, raise an error if the config file could not be modified.

--- kiwi/bootloader/install/grub2.py
+++ kiwi/bootloader/install/grub2.py
@@ -287,22 +287,26 @@ class BootLoaderInstallGrub2(BootLoaderInstallBase):
                         self._enable_grub2_install(self.root_mount.mountpoint)
 
     def set_disk_password(self, password: str):
-        if self.root_mount and password is not None:
-            log.debug('Include cryptomount credentials...')
-            config_file = '{0}/boot/efi/EFI/BOOT/grub.cfg'.format(
-                self.root_mount.mountpoint
-            )
-            if os.path.isfile(config_file):
-                with open(config_file) as config:
-                    grub_config = config.read()
-                    grub_config = re.sub(
-                        r'cryptomount',
-                        f'cryptomount -p "{password}"',
-                        grub_config
+        with ExitStack() as stack:
+            self._mount_device_and_volumes(stack)
+            if self.root_mount and password is not None:
+                log.debug('Include cryptomount credentials...')
+                config_file = f'{self.root_mount.mountpoint}/boot/efi/EFI/BOOT/grub.cfg'
+                if os.path.isfile(config_file):
+                    with open(config_file) as config:
+                        grub_config = config.read()
+                        grub_config = re.sub(
+                            r'cryptomount',
+                            f'cryptomount -p "{password}"',
+                            grub_config
+                        )
+                    with open(config_file, 'w') as grub_config_file:
+                        grub_config_file.write(grub_config)
+                        log.debug(f'<<< {grub_config} >>>')
+                else:
+                    raise KiwiBootLoaderGrubInstallError(
+                        f'grub2 config file {config_file} missing'
                     )
-                with open(config_file, 'w') as grub_config_file:
-                    grub_config_file.write(grub_config)
-                    log.debug(f'<<< {grub_config} >>>')
 
     def _mount_device_and_volumes(self, stack: ExitStack):
         self.root_mount = MountManager(
