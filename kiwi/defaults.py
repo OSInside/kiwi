e86e5423c0f1ef7ed53a94241145a5f5997c89dc
Marcus Schäfer <marcus.schaefer@gmail.com>
2024-07-30 18:16:54 +0200

    Do not exclude the .profile env file by default
    kiwi's initrd modules read a .profile file which gets included
    into the initrd produced at build time. To allow rebuild of a
    host-only initrd from the booted system this information should
    be present such that it is possible to re-use kiwi initrd code.

--- kiwi/defaults.py
+++ kiwi/defaults.py
@@ -397,7 +397,7 @@ class Defaults:
         :rtype: list
         """
         exclude_list = [
-            'image', '.profile', '.kconfig'
+            'image', '.kconfig'
         ]
         if no_tmpdirs:
             exclude_list += ['run/*', 'tmp/*']
