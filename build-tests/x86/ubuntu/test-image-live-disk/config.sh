a93da9fe9d5984517be002ca50d15dd04a9ea04b
Marcus Schäfer <marcus.schaefer@gmail.com>
2024-08-02 18:02:25 +0200

    Fix Debian/Ubuntu integration tests
    Remove package hacks for debootstrap, explicitly add
    required packages and or configurations.

--- build-tests/x86/ubuntu/test-image-live-disk/config.sh
+++ build-tests/x86/ubuntu/test-image-live-disk/config.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -ex
+
 #======================================
 # Functions...
 #--------------------------------------
@@ -9,56 +11,7 @@ test -f /.kconfig && . /.kconfig
 #--------------------------------------
 echo "Configure image: [${kiwi_iname}]..."
 
-# On Debian based distributions the kiwi built in way
-# to setup locale, keyboard and timezone via systemd tools
-# does not work because not(yet) provided by the distribution.
-# Thus the following manual steps to make the values provided
-# in the image description effective needs to be done.
-#
-#=======================================
-# Setup system locale
-#---------------------------------------
-echo "LANG=${kiwi_language}" > /etc/locale.conf
-
-#=======================================
-# Setup system keymap
-#---------------------------------------
-echo "KEYMAP=${kiwi_keytable}" > /etc/vconsole.conf
-echo "FONT=eurlatgr.psfu" >> /etc/vconsole.conf
-echo "FONT_MAP=" >> /etc/vconsole.conf
-echo "FONT_UNIMAP=" >> /etc/vconsole.conf
-
-#=======================================
-# Setup system timezone
-#---------------------------------------
-[ -f /etc/localtime ] && rm /etc/localtime
-ln -s /usr/share/zoneinfo/${kiwi_timezone} /etc/localtime
-
-#=======================================
-# Setup HW clock to UTC
-#---------------------------------------
-echo "0.0 0 0.0" > /etc/adjtime
-echo "0" >> /etc/adjtime
-echo "UTC" >> /etc/adjtime
-
-#======================================
-# Disable systemd NTP timesync
-#--------------------------------------
-baseRemoveService systemd-timesyncd
-
-#======================================
-# Enable firstboot resolv.conf setting
-#--------------------------------------
-baseInsertService symlink-resolvconf
-
 #======================================
 # Setup default target, multi-user
 #--------------------------------------
 baseSetRunlevel 3
-
-#======================================
-# Clear apt-get data
-#--------------------------------------
-apt-get clean
-rm -r /var/lib/apt/*
-rm -r /var/cache/apt/*
