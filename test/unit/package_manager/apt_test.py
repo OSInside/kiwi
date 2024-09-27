ad50bc7d329ffb51f858f4a8cc1747800f513334
Marcus Schäfer <marcus.schaefer@gmail.com>
2024-09-27 14:38:04 +0200

    Run package scripts in apt bootstrap phase
    The bootstrap procedure based on apt only runs a manual
    collection of package scripts. This commit refactors the
    code that unpacks the bootstrap packages to a python
    implementation and adds a method to run the bootstrap
    scripts from all packages resolved by apt.

--- test/unit/package_manager/apt_test.py
+++ test/unit/package_manager/apt_test.py
@@ -1,6 +1,5 @@
 import logging
 import io
-from textwrap import dedent
 from unittest.mock import (
     patch, call, Mock, MagicMock
 )
@@ -31,11 +30,12 @@ class TestPackageManagerApt:
         repository.keyring = 'trusted.gpg'
         repository.unauthenticated = 'false'
         repository.components = ['main', 'restricted']
+        self.env = {'key': 'val'}
 
         repository.runtime_config = Mock(
             return_value={
                 'apt_get_args': ['-c', 'apt.conf', '-y'],
-                'command_env': ['env'],
+                'command_env': self.env,
                 'distribution': 'xenial',
                 'distribution_path': 'xenial_path'
             }
@@ -79,12 +79,12 @@ class TestPackageManagerApt:
             bootstrap_package='bootstrap-package'
         )
         assert mock_Command_run.call_args_list == [
-            call(['apt-get', '-c', 'apt.conf', '-y', 'update'], ['env']),
+            call(['apt-get', '-c', 'apt.conf', '-y', 'update'], self.env),
             call(
                 [
                     'apt-get', '-c', 'apt.conf', '-y',
                     'install', 'bootstrap-package'
-                ], ['env']
+                ], self.env
             ),
             call(
                 [
@@ -116,9 +116,14 @@ class TestPackageManagerApt:
     @patch('kiwi.command.Command.run')
     @patch('kiwi.command.Command.call')
     @patch('kiwi.package_manager.apt.Temporary.new_file')
+    @patch('kiwi.package_manager.apt.Temporary.new_dir')
+    @patch('os.path.exists')
     def test_process_install_requests_bootstrap(
-        self, mock_Temporary_new_file, mock_Command_call, mock_Command_run
+        self, mock_os_path_exists, mock_Temporary_new_dir,
+        mock_Temporary_new_file, mock_Command_call, mock_Command_run
     ):
+        mock_os_path_exists.return_value = True
+        mock_Temporary_new_dir.return_value.name = 'tempdir'
         mock_Temporary_new_file.return_value.name = 'temporary'
         self.manager.request_package('vim')
         call_result = Mock()
@@ -127,31 +132,15 @@ class TestPackageManagerApt:
         with patch('builtins.open', create=True) as mock_open:
             mock_open.return_value = MagicMock(spec=io.IOBase)
             file_handle = mock_open.return_value.__enter__.return_value
+            file_handle.__iter__.return_value = ['base-passwd\n', 'usrmerge', 'vim\n']
             self.manager.process_install_requests_bootstrap()
-            script = dedent('''\n
-                set -e
-                while read -r deb;do
-                    echo "Unpacking $deb"
-                    dpkg-deb --fsys-tarfile $deb | tar -C {0} -x
-                done < {1}
-                while read -r deb;do
-                    pushd "$(dirname "$deb")" >/dev/null || exit 1
-                    if [[ "$(basename "$deb")" == base-passwd* ]];then
-                        echo "Running pre/post package scripts for $(basename "$deb")"
-                        dpkg -e "$deb" "{0}/DEBIAN"
-                        test -e {0}/DEBIAN/preinst && chroot {0} bash -c "/DEBIAN/preinst install"
-                        test -e {0}/DEBIAN/postinst && chroot {0} bash -c "/DEBIAN/postinst"
-                        rm -rf {0}/DEBIAN
-                    fi
-                    popd >/dev/null || exit 1
-                done < {1}
-            ''')
-            file_handle.write.assert_called_once_with(
-                script.format('root-dir', 'temporary')
-            )
+        new_env = self.env | {
+            'DPKG_MAINTSCRIPT_NAME': 'true',
+            'DPKG_MAINTSCRIPT_PACKAGE': 'libc6'
+        }
         assert mock_Command_run.call_args_list == [
             call(
-                ['apt-get', '-c', 'apt.conf', '-y', 'update'], ['env']
+                ['apt-get', '-c', 'apt.conf', '-y', 'update'], self.env
             ),
             call(
                 [
@@ -159,14 +148,74 @@ class TestPackageManagerApt:
                     '-oDebug::pkgDPkgPm=1',
                     '-oDPkg::Pre-Install-Pkgs::=cat >temporary',
                     '?essential',
-                    '?exact-name(usr-is-merged)',
-                    'base-passwd',
                     'vim',
                     'apt'
-                ], ['env']
+                ], self.env
             ),
             call(
-                ['bash', 'temporary'], ['env']
+                [
+                    'bash', '-c',
+                    'dpkg-deb --fsys-tarfile base-passwd | tar -C root-dir -x'
+                ]
+            ),
+            call(
+                [
+                    'bash', '-c',
+                    'dpkg-deb --fsys-tarfile usrmerge | tar -C root-dir -x'
+                ], self.env
+            ),
+            call(
+                [
+                    'bash', '-c',
+                    'dpkg-deb --fsys-tarfile vim | tar -C root-dir -x'
+                ]
+            ),
+            call(
+                [
+                    'dpkg', '-e', 'base-passwd', 'tempdir/base-passwd'
+                ]
+            ),
+            call(
+                [
+                    'chroot', 'root-dir', 'bash',
+                    'tempdir/base-passwd/preinst', 'install'
+                ], new_env
+            ),
+            call(
+                [
+                    'chroot', 'root-dir', 'bash',
+                    'tempdir/base-passwd/postinst', 'configure'
+                ], new_env
+            ),
+            call(
+                ['dpkg', '-e', 'base-passwd', 'tempdir/base-passwd']
+            ),
+            call(
+                [
+                    'chroot', 'root-dir', 'bash',
+                    'tempdir/base-passwd/preinst', 'install'
+                ], new_env
+            ),
+            call(
+                [
+                    'chroot', 'root-dir', 'bash',
+                    'tempdir/base-passwd/postinst', 'configure'
+                ], new_env
+            ),
+            call(
+                ['dpkg', '-e', 'vim', 'tempdir/vim']
+            ),
+            call(
+                [
+                    'chroot', 'root-dir', 'bash',
+                    'tempdir/vim/preinst', 'install'
+                ], new_env
+            ),
+            call(
+                [
+                    'chroot', 'root-dir', 'bash',
+                    'tempdir/vim/postinst', 'configure'
+                ], new_env
             )
         ]
 
@@ -178,7 +227,7 @@ class TestPackageManagerApt:
         mock_call.assert_called_once_with([
             'chroot', 'root-dir', 'apt-get',
             '-c', 'apt.conf', '-y', 'install', 'vim'],
-            ['env']
+            self.env
         )
 
     @patch('kiwi.command.Command.call')
@@ -190,7 +239,7 @@ class TestPackageManagerApt:
             [
                 'chroot', 'root-dir', 'apt-get', '-c', 'apt.conf', '-y',
                 '--auto-remove', 'remove', 'vim'
-            ], ['env']
+            ], self.env
         )
 
     @patch('kiwi.command.Command.call')
@@ -228,7 +277,7 @@ class TestPackageManagerApt:
                 '--remove', '--force-remove-reinstreq',
                 '--force-remove-essential', '--force-depends', 'vim'
             ],
-            ['env']
+            self.env
         )
         mock_iglob.call_args_list == [
             call('root-dir/var/lib/dpkg/info/vim*.pre*'),
@@ -263,7 +312,7 @@ class TestPackageManagerApt:
         mock_call.assert_called_once_with([
             'chroot', 'root-dir', 'apt-get',
             '-c', 'apt.conf', '-y', 'upgrade'],
-            ['env']
+            self.env
         )
 
     def test_process_only_required(self):
