531df29c049b76cd9bf632158f83b409885d8358
Marcus Schäfer <marcus.schaefer@gmail.com>
2025-06-25 14:21:01 +0200

    Bump version: 10.2.24 → 10.2.25

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -142,7 +142,7 @@ copyright = f'2020-{datetime.now().year}, {author}'
 # built documents.
 #
 # The short X.Y version.
-version = '10.2.24'
+version = '10.2.25'
 # The full version, including alpha/beta/rc tags.
 release = version
 
