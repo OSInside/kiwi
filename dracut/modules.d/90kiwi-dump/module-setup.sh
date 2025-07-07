282a4e1491e8582911abe57e0521a14eacf24c57
Marcus Schäfer <marcus.schaefer@gmail.com>
2025-07-07 16:35:27 +0200

    Add rd.kiwi.install.devicepersistency
    Allow to specify which type of persistent device name should
    be used to build up the list of installation disk devices.
    For example rd.kiwi.install.devicepersistency=by-path would
    use the by-path representations for the available disk
    devices. The default (by-id) stays untouched. In case an
    invalid or not present device representation is selected, kiwi
    falls back to the non persistent unix node names.

--- dracut/modules.d/90kiwi-dump/module-setup.sh
+++ dracut/modules.d/90kiwi-dump/module-setup.sh
@@ -21,7 +21,7 @@ install() {
     declare moddir=${moddir}
     declare systemdutildir=${systemdutildir}
     inst_multiple \
-        tr lsblk dd sha256sum head pv kexec basename awk kpartx
+        tr lsblk dd sha256sum head pv kexec basename awk kpartx sort
 
     inst_hook pre-udev 30 "${moddir}/kiwi-installer-genrules.sh"
 
