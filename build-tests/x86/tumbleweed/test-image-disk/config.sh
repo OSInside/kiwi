d4d26556f4fe9c2d5e1be449068cda92265047b1
Marcus Schäfer <marcus.schaefer@gmail.com>
2025-10-24 20:51:43 +0200

    Add integration test build for retain feature

--- build-tests/x86/tumbleweed/test-image-disk/config.sh
+++ build-tests/x86/tumbleweed/test-image-disk/config.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 set -ex
 
+declare kiwi_profiles=${kiwi_profiles}
+
 #======================================
 # Activate services
 #--------------------------------------
 systemctl enable sshd
 systemctl enable grub_config
 systemctl enable dracut_hostonly
+
+# Just in case the kiwi resizer is disabled in the system and
+# gets dropped from the initrd by the customer for some reason
+for profile in ${kiwi_profiles//,/ }; do
+    if [ "${profile}" = "Retain" ]; then
+        cat > /etc/fstab.script <<-EOF
+		sed -ie "s@/home ext4 defaults@/home ext4 x-systemd.growfs,defaults@" /etc/fstab
+        rm /etc/fstabe
+		EOF
+    fi
+done
