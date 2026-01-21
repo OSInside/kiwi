e8280b03b3ef4d795d8fa6619c2a463f1da5c0d4
Marcus Schäfer <marcus.schaefer@gmail.com>
2026-01-21 16:49:06 +0100

    update opencode PATH

--- build-tests/x86/tumbleweed/test-image-opencode/config.sh
+++ build-tests/x86/tumbleweed/test-image-opencode/config.sh
@@ -3,5 +3,9 @@ set -ex
 
 mkdir -p /root/ai
 
+echo 'export PATH=/root/.opencode/bin:$PATH' > /root/.bashrc 
+
 curl -fsSL https://opencode.ai/install > opencode.install.sh
 bash opencode.install.sh
+
+test -d /root/.opencode
