ee0b2113dba96f021d207fc6c6c70b545f4ed650
Marcus Schäfer <marcus.schaefer@gmail.com>
2026-02-18 10:28:52 +0100

    Add Micro Build with extra data partition

--- build-tests/x86/tumbleweed/test-image-MicroOS/config.sh
+++ build-tests/x86/tumbleweed/test-image-MicroOS/config.sh
@@ -13,6 +13,15 @@ test -f /.kconfig && . /.kconfig
 echo FONT="eurlatgr.psfu" >> /etc/vconsole.conf
 
 #======================================
+# Add data partition mountpoint
+#--------------------------------------
+for profile in ${kiwi_profiles//,/ }; do
+    if [ "${profile}" = "DataPart" ]; then
+        mkdir -p /data
+    fi
+done
+
+#======================================
 # prepare for setting root pw, timezone
 #--------------------------------------
 echo "** reset machine settings"
@@ -42,33 +51,35 @@ EOF
 
 # Add repos from /etc/YaST2/control.xml
 if [ -x /usr/sbin/add-yast-repos ]; then
-	add-yast-repos
-	zypper --non-interactive rm -u live-add-yast-repos
+    add-yast-repos
+    zypper --non-interactive rm -u live-add-yast-repos
 fi
 
 #=====================================
 # Configure snapper
 #-------------------------------------
 if [ "${kiwi_btrfs_root_is_snapshot-false}" = 'true' ]; then
-        echo "creating initial snapper config ..."
-        cp /usr/share/snapper/config-templates/default /etc/snapper/configs/root
-        baseUpdateSysConfig /etc/sysconfig/snapper SNAPPER_CONFIGS root
+    echo "creating initial snapper config ..."
+    cp /usr/share/snapper/config-templates/default /etc/snapper/configs/root
+    baseUpdateSysConfig /etc/sysconfig/snapper SNAPPER_CONFIGS root
 
-	# Adjust parameters
-	sed -i'' 's/^TIMELINE_CREATE=.*$/TIMELINE_CREATE="no"/g' /etc/snapper/configs/root
-	sed -i'' 's/^NUMBER_LIMIT=.*$/NUMBER_LIMIT="2-10"/g' /etc/snapper/configs/root
-	sed -i'' 's/^NUMBER_LIMIT_IMPORTANT=.*$/NUMBER_LIMIT_IMPORTANT="4-10"/g' /etc/snapper/configs/root
+    # Adjust parameters
+    sed -i'' 's/^TIMELINE_CREATE=.*$/TIMELINE_CREATE="no"/g' \
+        /etc/snapper/configs/root
+    sed -i'' 's/^NUMBER_LIMIT=.*$/NUMBER_LIMIT="2-10"/g' \
+        /etc/snapper/configs/root
+    sed -i'' 's/^NUMBER_LIMIT_IMPORTANT=.*$/NUMBER_LIMIT_IMPORTANT="4-10"/g' \
+        /etc/snapper/configs/root
 fi
 
 #=====================================
 # Enable chrony if installed
 #-------------------------------------
 if [ -f /etc/chrony.conf ]; then
-	systemctl enable chronyd
+    systemctl enable chronyd
 fi
 
-
 # To make x-systemd.growfs work from inside the initrd
-cat >/etc/dracut.conf.d/50-microos-growfs.conf <<"EOF"
+cat >/etc/dracut.conf.d/50-microos-growfs.conf <<EOF
 install_items+=" /usr/lib/systemd/systemd-growfs "
 EOF
