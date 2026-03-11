c87094e425cdfd509040cfd77f933e047fd3fb57
Marcus Schäfer <marcus.schaefer@gmail.com>
2026-03-11 15:41:20 +0100

    Bump version: 10.2.43 → 10.2.44

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -143,7 +143,7 @@ copyright = f'2020-{datetime.now().year}, {author}'
 # built documents.
 #
 # The short X.Y version.
-version = '10.2.43'
+version = '10.2.44'
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- kiwi/version.py
+++ kiwi/version.py
@@ -18,5 +18,5 @@
 """
 Global version information used in kiwi and the package
 """
-__version__ = '10.2.43'
+__version__ = '10.2.44'
 __githash__ = '$Format:%H$'
