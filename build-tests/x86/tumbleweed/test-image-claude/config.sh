bcdf2d0bd5421c6ccdd170f279fa576ba161c164
Marcus Schäfer <marcus.schaefer@gmail.com>
2026-05-05 16:24:49 +0200

    Use npm to install claude

--- build-tests/x86/tumbleweed/test-image-claude/config.sh
+++ build-tests/x86/tumbleweed/test-image-claude/config.sh
@@ -3,9 +3,4 @@ set -ex
 
 mkdir -p /root/ai
 
-echo 'export PATH=/root/.claude/bin:$PATH' > /root/.bashrc 
-
-curl -fsSL https://claude.ai/install.sh > claude.install.sh
-bash claude.install.sh
-
-test -d /root/.claude
+npm install -g @anthropic-ai/claude-code
