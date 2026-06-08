e5d267ff3d5a62829de67b426eb5273e9ea11a77
Marcus Schäfer <marcus.schaefer@gmail.com>
2026-06-08 18:31:41 +0200

    move to antigravity because gemini-cli is dead

--- build-tests/x86/tumbleweed/test-image-gemini/config.sh
+++ build-tests/x86/tumbleweed/test-image-gemini/config.sh
@@ -3,6 +3,9 @@ set -ex
 
 mkdir -p /root/ai
 
-npm install -g @google/gemini-cli@nightly
+echo 'export PATH=/root/.antigravity/bin:/root/.local/bin:$PATH' > /root/.bashrc
+source /root/.bashrc
 
-#npm install -g @google/gemini-cli@latest
+curl -fsSL https://antigravity.google/cli/install.sh > antigravity.install.sh
+
+bash antigravity.install.sh
