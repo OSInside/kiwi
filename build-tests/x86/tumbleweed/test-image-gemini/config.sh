9eb569794e6a15ece690dd129675efb3f1a25b9e
Marcus Schäfer <marcus.schaefer@gmail.com>
2026-06-10 17:31:46 +0200

    Keep both gemini-cli and antigravity

--- build-tests/x86/tumbleweed/test-image-gemini/config.sh
+++ build-tests/x86/tumbleweed/test-image-gemini/config.sh
@@ -6,6 +6,9 @@ mkdir -p /root/ai
 echo 'export PATH=/root/.antigravity/bin:/root/.local/bin:$PATH' > /root/.bashrc
 source /root/.bashrc
 
+# install antigravity
 curl -fsSL https://antigravity.google/cli/install.sh > antigravity.install.sh
-
 bash antigravity.install.sh
+
+# install gemini-cli
+npm install -g @google/gemini-cli@nightly
