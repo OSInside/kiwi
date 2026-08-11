af05181d4cafd9c2b9bb319a90e0be8fcc00ad04
Jesus Bermudez Velazquez <jesusbv@suse.com>
2026-08-11 18:13:50 +0100

    add settings for claude
    add claude settings.json file
    
    add google-cloud-cli to get the token
    
    add vim in case we want to edit files

--- build-tests/x86/tumbleweed/test-image-claude/config.sh
+++ build-tests/x86/tumbleweed/test-image-claude/config.sh
@@ -4,3 +4,13 @@ set -ex
 mkdir -p /root/ai
 
 npm install -g @anthropic-ai/claude-code
+
+zypper ar -f https://packages.cloud.google.com/yum/repos/cloud-sdk-el10-x86_64 google-cloud-rhel10
+
+zypper ar  https://download.opensuse.org/tumbleweed/repo/oss TW
+
+sudo zypper in google-cloud-cli
+
+mkdir -p ~/.claude
+
+touch ~/.claude/settings.json
