6733335a0c2748985a0b38eca5b01f3774e5cea3
Marcus Schäfer <marcus.schaefer@gmail.com>
2026-08-12 10:06:34 +0200

    Cleanup shellcheck issues

--- build-tests/x86/tumbleweed/test-image-gemini/config.sh
+++ build-tests/x86/tumbleweed/test-image-gemini/config.sh
@@ -3,8 +3,13 @@ set -ex
 
 mkdir -p /root/ai
 
+# shellcheck disable=SC2016
 echo 'export PATH=/root/.antigravity/bin:/root/.local/bin:$PATH' > /root/.bashrc
+
+# shellcheck disable=SC2016
 echo 'export PATH=/root/.antigravity/bin:/root/.local/bin:$PATH' >> /etc/bash.bashrc
+
+# shellcheck disable=SC1091
 source /root/.bashrc
 
 # install antigravity
--- build-tests/x86/tumbleweed/test-image-opencode/config.sh
+++ build-tests/x86/tumbleweed/test-image-opencode/config.sh
@@ -3,6 +3,7 @@ set -ex
 
 mkdir -p /root/ai
 
+# shellcheck disable=SC2016
 echo 'export PATH=/root/.opencode/bin:$PATH' > /root/.bashrc 
 
 curl -fsSL https://opencode.ai/install > opencode.install.sh
