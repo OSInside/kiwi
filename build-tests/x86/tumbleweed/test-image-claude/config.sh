11675b9af19fb48d5b5008dbae3bcbe123fec1ae
Marcus Schäfer <marcus.schaefer@gmail.com>
2026-08-12 10:46:49 +0200

    Fix claude install

--- build-tests/x86/tumbleweed/test-image-claude/config.sh
+++ build-tests/x86/tumbleweed/test-image-claude/config.sh
@@ -3,14 +3,13 @@ set -ex
 
 mkdir -p /root/ai
 
-npm approve-scripts @anthropic-ai/claude-code
-npm install -g @anthropic-ai/claude-code
-
 zypper ar -f https://packages.cloud.google.com/yum/repos/cloud-sdk-el10-x86_64 google-cloud-rhel10
 
-zypper ar  https://download.opensuse.org/tumbleweed/repo/oss TW
+zypper ar -f https://download.opensuse.org/tumbleweed/repo/oss TW
 
 zypper --non-interactive --gpg-auto-import-keys in --auto-agree-with-licenses --allow-vendor-change google-cloud-cli
+
+npm install -g @anthropic-ai/claude-code
 
 mkdir -p ~/.claude
 
