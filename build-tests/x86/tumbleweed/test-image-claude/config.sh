19bae6f2106dfaac39af7c71c7b6111608288641
Marcus Schäfer <marcus.schaefer@gmail.com>
2026-08-12 10:30:00 +0200

    non interactive zypper calls

--- build-tests/x86/tumbleweed/test-image-claude/config.sh
+++ build-tests/x86/tumbleweed/test-image-claude/config.sh
@@ -9,7 +9,7 @@ zypper ar -f https://packages.cloud.google.com/yum/repos/cloud-sdk-el10-x86_64 g
 
 zypper ar  https://download.opensuse.org/tumbleweed/repo/oss TW
 
-zypper in google-cloud-cli
+zypper --non-interactive --gpg-auto-import-keys in --auto-agree-with-licenses --allow-vendor-change google-cloud-cli
 
 mkdir -p ~/.claude
 
